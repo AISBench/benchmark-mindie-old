a49449af012eb8ad49947db3807284bb074fa919
Hanye <1037452625@qq.com>
2026-02-02 11:01:40 +0800

    Create SuperGLUE_BoolQ_gen_0_shot_str.py

---
+++ mindie_llm_examples/dataset_config/SuperGLUE_BoolQ_gen_0_shot_str.py
@@ -0,0 +1,49 @@
+from ais_bench.benchmark.openicl.icl_prompt_template import PromptTemplate
+from ais_bench.benchmark.openicl.icl_retriever import ZeroRetriever
+from ais_bench.benchmark.openicl.icl_inferencer import GenInferencer
+from ais_bench.benchmark.openicl.icl_evaluator import AccEvaluator
+from ais_bench.benchmark.datasets import BoolQDatasetV2
+from ais_bench.benchmark.utils.text_postprocessors import first_capital_postprocess
+
+def bool_q_postprocess(pred: str):
+    processed_pred = first_capital_postprocess(pred)
+    if processed_pred in ["A", "B"]:
+        return processed_pred
+    import re
+    pattern = r'\b(Yes|No)\b'
+    matches = re.findall(pattern, pred)
+    if matches[0] = "Yes":
+        return "A"
+    else:
+        return "B"
+      
+BoolQ_reader_cfg = dict(
+    input_columns=['question', 'passage'],
+    output_column='label',
+)
+
+BoolQ_infer_cfg = dict(
+    prompt_template=dict(
+        type=PromptTemplate,
+        template='{passage}\nQuestion: {question}\nA. Yes\nB. No\nAnswer:'
+    ),
+    retriever=dict(type=ZeroRetriever),
+    inferencer=dict(type=GenInferencer),
+)
+
+BoolQ_eval_cfg = dict(
+    evaluator=dict(type=AccEvaluator),
+    pred_role='BOT',
+    pred_postprocessor=dict(type=bool_q_postprocess),
+)
+
+BoolQ_datasets = [
+    dict(
+        abbr='BoolQ',
+        type=BoolQDatasetV2,
+        path='ais_bench/datasets/SuperGLUE/BoolQ/val.jsonl',  # 数据集路径，使用相对路径时相对于源码根路径，支持绝对路径
+        reader_cfg=BoolQ_reader_cfg,
+        infer_cfg=BoolQ_infer_cfg,
+        eval_cfg=BoolQ_eval_cfg,
+    )
+]
