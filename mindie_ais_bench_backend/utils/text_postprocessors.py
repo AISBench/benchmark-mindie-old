a6738cf93e97dc6c094d698e6946203a038390b9
Hanye <1037452625@qq.com>
2026-02-02 11:21:03 +0800

    Update text_postprocessors.py

--- mindie_ais_bench_backend/utils/text_postprocessors.py
+++ mindie_ais_bench_backend/utils/text_postprocessors.py
@@ -1,4 +1,6 @@
 from ais_bench.benchmark.registry import TEXT_POSTPROCESSORS
+from ais_bench.benchmark.utils.text_postprocessors import first_capital_postprocess
+
 @TEXT_POSTPROCESSORS.register_module('bool_q_postprocess')
 def bool_q_postprocess(pred: str):
     processed_pred = first_capital_postprocess(pred)
